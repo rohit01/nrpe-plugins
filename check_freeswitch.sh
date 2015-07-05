4d23dfc8cd5b156263b4d5c209c1a6558c1005ce
rohit01 <rohit.kgec@gmail.com>
2015-07-05 15:24:43 +0530

    Added new features for FS checks: pri_status, calls_count

--- check_freeswitch.sh
+++ check_freeswitch.sh
@@ -5,6 +5,7 @@
 #
 # Actual fs_cli commands being run to perform checks:
 # show channels count: /usr/local/freeswitch/bin/fs_cli -q -x "show channels count" -t 2000
+# show calls count: /usr/local/freeswitch/bin/fs_cli -q -x "show calls count" -t 2000
 # status: /usr/local/freeswitch/bin/fs_cli -q -x "status" -t 2000
 # pri_metrics: /usr/local/freeswitch/bin/fs_cli -q -x "ftdm list" -t 2000
 #              /usr/local/freeswitch/bin/fs_cli -q -x "ftdm core calls" -t 2000
@@ -20,6 +21,7 @@ ST_WR=1
 ST_CR=2
 ST_UK=3
 PRI_HISTORY_FILE="/tmp/._pri_history_${PROGNAME}"
+LAST_CALLS_COUNT_FILE="/tmp/._last_calls_count_${PROGNAME}"
 
 ## Global variables & default settings
 fs_command="$1"
@@ -39,7 +41,8 @@ print_help() {
     echo "$PROGNAME is a custom NRPE plugin to check freeswitch status"
     echo "using fs_cli command."
     echo "Freeswitch status check commands defined in this module are:"
-    echo "'show channels count', 'status', 'pri_metrics'"
+    echo "'show channels count', 'show calls count', 'status', 'pri_metrics', "
+    echo "'pri_status', 'calls_count'"
     echo ""
     echo "Usage: $PROGNAME 'show channels count' [-w 80] [-c 90]"
     echo ""
@@ -48,12 +51,16 @@ print_help() {
     echo "     Defines a warning level. Not applicable for 'status' command"
     echo "     Command: "
     echo "        'show channels count': No of channels in use. Default: 300"
+    echo "        'show calls count': No of calls in use. Default: 300"
     echo "        'pri_metrics': No of seconds span is not in use. Default: 300"
+    echo "        'calls_count': Zero calls for x duration. Default: 300 secs"
     echo "  -c/--critical)"
     echo "     Defines a critical level. Not applicable for 'status' command"
     echo "     Command: "
     echo "        'show channels count': No of channels in use. Default: 400"
+    echo "        'show calls count': No of calls in use. Default: 400"
     echo "        'pri_metrics': No of seconds span is not in use. Default: 400"
+    echo "        'calls_count': Zero calls for x duration. Default: 400 secs"
     echo "  -h/--help)"
     echo "     Print this help message & exit"
     echo "  -v/--version)"
@@ -62,6 +69,7 @@ print_help() {
     echo "Examples:"
     echo "   $PROGNAME 'status'"
     echo "   $PROGNAME 'show channels count' -w 500 -c 650"
+    echo "   $PROGNAME 'show calls count' -w 500 -c 650"
     echo "   $PROGNAME 'pri_metrics' -w 600 -c 900"
 }
 
@@ -148,6 +156,72 @@ get_pri_perf_data() {
     done | tr '\n' ' ' | sed 's/ *$//'
 }
 
+get_pri_status_summary() {
+    span_details="${1}"
+    wan_status="${2}"
+    total_spans="$(echo "${span_details}" | grep "^span: " | wc -l)"
+    pok_count="$(echo "${span_details}" | grep "^physical_status: ok" | wc -l)"
+    sup_count="$(echo "${span_details}" | grep "^signaling_status: UP" | wc -l)"
+    (
+        echo "physical_ok:${pok_count}/${total_spans} signal_up:${sup_count}/${total_spans}. Details:"
+        echo "${span_details}" | grep "^span: " | while read span_line
+        do
+            span_no="$(echo "${span_line}" | cut -d" " -f 2)"
+            connect_status="$(echo "${wan_status}" | grep "^wanpipe${span_no} " | cut -d"|" -f 4 | sed 's/^\s*//' | sed 's/\s*$//')"
+            if [ "X${connect_status}" == "X" ]; then
+                connect_status="NotFound"
+            fi
+            physical_status="$(echo "${span_details}" | grep -A 3 "^${span_line}$" | grep "^physical_status: " | sed 's/^physical_status: //')"
+            signaling_status="$(echo "${span_details}" | grep -A 3 "^${span_line}$" | grep "^signaling_status: " | sed 's/^signaling_status: //')"
+            if [ "X${connect_status}" != "XNotFound" ] && [ "X${physical_status}" == "Xok" ] && [ "X${connect_status}" != "XConnected" ]; then
+                echo "CRITICAL:span-${span_no}:${physical_status}:${signaling_status}:${connect_status}"
+            elif [ "X${connect_status}" != "XNotFound" ] && [ "X${physical_status}" != "Xok" ] && [ "X${connect_status}" == "XConnected" ]; then
+                echo "CRITICAL:span-${span_no}:${physical_status}:${signaling_status}:${connect_status}"
+            elif [ "X${physical_status}" == "Xok" ] && [ "X${signaling_status}" != "XUP" ]; then
+                echo "CRITICAL:span-${span_no}:${physical_status}:${signaling_status}:${connect_status}"
+            elif [ "X${physical_status}" != "Xok" ] && [ "X${signaling_status}" == "XUP" ]; then
+                echo "CRITICAL:span-${span_no}:${physical_status}:${signaling_status}:${connect_status}"
+            else
+                echo "OK:span-${span_no}:${physical_status}:${signaling_status}:${connect_status}"
+            fi
+        done
+        echo "| physical_ok=${pok_count};${total_spans} signal_up=${sup_count};${total_spans}"
+    ) | tr '\n' ' ' | sed 's/ *$//'
+}
+
+calls_count_alert_summary() {
+    no_of_calls="${1}"
+    time_now="$(date +%s)"
+    if ((${no_of_calls} > 0)); then
+        echo "${time_now} ${no_of_calls}" > "${LAST_CALLS_COUNT_FILE}"
+        echo "OK - Total calls: ${no_of_calls}"
+        exit "${ST_OK}"
+    else
+        last_status="$(cat "${LAST_CALLS_COUNT_FILE}" 2>/dev/null || echo '')"
+        if [ "X${last_status}" == "X" ]; then
+            last_zero_time="$(date +%s)"
+        else
+            if (($(echo "${last_status}" | cut -d" " -f 2) == 0)); then
+                last_zero_time="$(echo "${last_status}" | cut -d" " -f 1)"
+            else
+                last_zero_time="$(date +%s)"
+            fi
+        fi
+        echo "${last_zero_time} ${no_of_calls}" > "${LAST_CALLS_COUNT_FILE}"
+        # Calculate alert and print summary
+        time_diff="$((${time_now} - ${last_zero_time}))"
+        if ((${time_diff} >= ${critical})); then
+            echo "CRITICAL - Zero calls since last ${time_diff} seconds (${time_diff} > ${critical})"
+            exit "${ST_CR}"
+        elif ((${time_diff} >= ${warning})); then
+            echo "WARNING - Zero calls since last ${time_diff} seconds (${time_diff} > ${warning})"
+            exit "${ST_WR}"
+        else
+            echo "OK - Zero calls since last ${time_diff} seconds (${time_diff} <= ${warning})"
+            exit "${ST_OK}"
+        fi
+    fi
+}
 
 #### EXECUTE CHECK ############################################################
 ## Get fs_cli command (1st argument)
@@ -164,14 +238,19 @@ case "${fs_command}" in
 esac
 if [ "X${fs_command}" == "X" ]; then
     echo "Status check command not specified. Valid commands are: "
-    echo "'show channels count' & 'status'"
+    echo "'show channels count', 'show calls count', 'pri_metrics', 'status',"
+    echo " 'pri_status' & 'calls_count'"
     echo "Use -h/--help option to get more details"
     exit_formalities "${message}" "${ST_UK}"
 elif [ "X${fs_command}" != "Xstatus" ] && \
         [ "X${fs_command}" != "Xshow channels count" ] && \
+        [ "X${fs_command}" != "Xshow calls count" ] && \
+        [ "X${fs_command}" != "Xpri_status" ] && \
+        [ "X${fs_command}" != "Xcalls_count" ] && \
         [ "X${fs_command}" != "Xpri_metrics" ]; then
     echo "Invalid command - '${fs_command}'. Valid commands are: "
-    echo "'show channels count' & 'status'"
+    echo "'show channels count', 'show calls count', 'pri_metrics', "
+    echo "'pri_status', 'calls_count' & 'status'"
     echo "Use -h/--help option to get more details"
     exit_formalities "${message}" "${ST_UK}"
 fi
@@ -234,19 +313,24 @@ if [ "X${fs_command}" == "Xstatus" ]; then
     message="OK - FS version: ${version}, git ID: ${git_commit}, Uptime: ${uptime}"
     exit_formalities "${message}" "${ST_OK}"
 
-elif [ "X${fs_command}" == "Xshow channels count" ]; then
+elif [ "X${fs_command}" == "Xshow channels count" ] || [ "X${fs_command}" == "Xshow calls count" ]; then
     ## Execute test for fs command - show channels count
     execute_check
     channels="$(echo "${fscli_output}" | tail -n 1 | tr -s ' ' | cut -d' ' -f 1 | sed 's/^ *//g' | sed 's/ *$//g')"
     perf_data="channels=${channels};${warning};${critical}"
+    if [ "X${fs_command}" == "Xshow channels count" ]; then
+        check_type="channels"
+    else
+        check_type="calls"
+    fi
     if [ "${channels}" -ge "${critical}" ]; then
-        message="CRITICAL - No of Channels in use: ${channels} | ${perf_data}"
+        message="CRITICAL - No of ${check_type} in use: ${channels} | ${perf_data}"
         exit_formalities "${message}" "${ST_CR}"
     elif [ "${channels}" -ge "${warning}" ]; then
-        message="WARNING - No of Channels in use: ${channels} | ${perf_data}"
+        message="WARNING - No of ${check_type} in use: ${channels} | ${perf_data}"
         exit_formalities "${message}" "${ST_WR}"
     else
-        message="Ok - No of Channels in use: ${channels} | ${perf_data}"
+        message="Ok - No of ${check_type} in use: ${channels} | ${perf_data}"
         exit_formalities "${message}" "${ST_OK}"
     fi
 
@@ -278,4 +362,26 @@ elif [ "X${fs_command}" == "Xpri_metrics" ]; then
         message="OK - Total calls: ${total_calls} | ${perf_data}"
         exit_formalities "${message}" "${exit_status}"
     fi
+
+elif [ "X${fs_command}" == "Xpri_status" ]; then
+    ## Execute PRI Status check
+    fs_command="ftdm list"
+    execute_check
+    span_details="${fscli_output}"
+    wan_status="$(cat /proc/net/wanrouter/status 2>/dev/null || echo "")"
+    message="$(get_pri_status_summary "${span_details}" "${wan_status}")"
+    if echo "${message}" | grep " CRITICAL:" >/dev/null; then
+        exit_formalities "CRITICAL - ${message}" "${ST_CR}"
+    else
+        exit_formalities "OK - ${message}" "${ST_OK}"
+    fi
+
+elif [ "X${fs_command}" == "Xcalls_count" ]; then
+    ## Execute calls count check
+    fs_command="show calls count"
+    execute_check
+    no_of_calls="$(echo "${fscli_output}" | grep 'total' | cut -d" " -f 1)"
+    message="$(calls_count_alert_summary "${no_of_calls}")"
+    exit_status="$?"
+    exit_formalities "${message}" "${exit_status}"
 fi
