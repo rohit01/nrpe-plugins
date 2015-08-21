4937d57fd6accf3b24b05464163629fafcf85391
rohit01 <rohit.kgec@gmail.com>
2015-08-21 07:49:19 +0530

    Warning & critical calculation added

--- cassandra_health.sh
+++ cassandra_health.sh
@@ -13,6 +13,10 @@ ST_WR=1
 ST_CR=2
 ST_UK=3
 
+## Defaults
+warning=80
+critical=50
+
 
 print_version() {
     echo "$PROGNAME: $VERSION $AUTHOR"
@@ -27,17 +31,25 @@ print_help() {
     echo "Usage: $PROGNAME -t 'show channels count' [-w 80] [-c 90]"
     echo ""
     echo "Options:"
+    echo "  -t/--checktype)"
+    echo "     Type of check to perform. Valid examples: status & token"
+    echo "  -w/--warning)"
+    echo "     token: warning is percent of tokens up. Default: 80"
+    echo "  -c/--critical)"
+    echo "     token: critical is percent of tokens up. Default: 50"
     echo "  -h/--help)"
     echo "     Print this help message & exit"
     echo "  -v/--version)"
     echo "     Print version of this script & exit"
     echo ""
     echo "Examples:"
-    echo "   $PROGNAME -t 'status'"
+    echo "   $PROGNAME -t 'status' [-w 80 -c 50]"
 }
 
 exit_formalities() {
-    echo "$1"
+    if [ "X$1" != "X" ]; then
+        echo "$1"
+    fi
     exit "$2"
 }
 
@@ -82,8 +94,28 @@ formatted_status_message() {
     echo "Version:${version} uptime:${uptime} load:${load} heap_mem:${heap_mem}mb off_heap_mem:${off_heap_mem}mb gossip:${gossip} thrift:${thrift} native_transport:${native_transport} | load=${load} heap_mem:${heap_mem}MB off_heap_mem:${off_heap_mem}mb"
 }
 
+calc_tokens_health() {
+    node_ring="$1"
+    tokens_up="$(echo "${node_ring}" | grep -c ' Up ')"
+    tokens_down="$(echo "${node_ring}" | grep -c ' Down ')"
+    total_tokens="$((${tokens_up} + ${tokens_down}))"
+    percent_tokens_up="$((${tokens_up} * 100 / ${total_tokens}))"
+    warning_count="$((${warning} * ${total_tokens} / 100))"
+    critical_count="$((${critical} * ${total_tokens} / 100))"
+    perf_info="tokens_up=${tokens_up};${warning_count};${critical_count} tokens_down=${tokens_down} total_tokens=${total_tokens}"
+    if [ "${percent_tokens_up}" -le "${critical}" ]; then
+        echo "CRITICAL - Tokens up: ${tokens_up}/${total_tokens} (${percent_tokens_up}%) | ${perf_info}"
+        exit "${ST_CR}"
+    elif [ "${percent_tokens_up}" -le "${warning}" ]; then
+        echo "WARNING - Tokens up: ${tokens_up}/${total_tokens} (${percent_tokens_up}%) | ${perf_info}"
+        exit "${ST_WR}"
+    else
+        echo "OK - Tokens up: ${tokens_up}/${total_tokens} (${percent_tokens_up}%) | ${perf_info}"
+        exit "${ST_OK}"
+    fi
+}
 
-## Get option arguments
+#### GET OPTION ARGUMENTS #####################################################
 while test -n "$1"; do
     case "$1" in
         --help|-h)
@@ -118,19 +150,22 @@ done
 
 #### VALIDATION ###############################################################
 if [ "X${checktype}" == "X" ]; then
-    echo "Mandatory argument 'checktype' not provided"
+    echo "ERROR: Mandatory argument '--checktype' not provided"
     echo "Use -h/--help option to get more details"
     exit_formalities "${message}" "${ST_UK}"
 elif [ "X${checktype}" != "Xstatus" ] && 
         [ "X${checktype}" != "Xtoken" ]; then
-    echo "Invalid value for checktype - '${checktype}'. Valid values are: "
+    echo "ERROR: Invalid value for checktype - '${checktype}'. Valid values are: "
     echo "'status', 'token'"
     echo "Use -h/--help option to get more details"
     exit_formalities "${message}" "${ST_UK}"
 fi
+if [ ${warning} -lt ${critical} ]; then
+    echo "ERROR: Value for --warning (${warning}%) must be greater than --critical (${critical}%)"
+    exit_formalities "${message}" "${ST_UK}"
+fi
 
 #### EXECUTE CHECK ############################################################
-
 if [ "X${checktype}" == "Xstatus" ]; then
     node_version="$(execute_nodetool version)"
     node_info="$(execute_nodetool info)"
@@ -140,9 +175,6 @@ if [ "X${checktype}" == "Xstatus" ]; then
 
 elif [ "X${checktype}" == "Xtoken" ]; then
     node_ring="$(execute_nodetool ring)"
-    tokens_up="$(echo "${node_ring}" | grep -c ' Up ')"
-    tokens_down="$(echo "${node_ring}" | grep -c ' Down ')"
-    total_tokens="$(expr ${tokens_up} + ${tokens_down})"
-    message="tokens_up:${tokens_up} tokens_down:${tokens_down} total_tokens:${total_tokens}"
-    exit_formalities "${message}" "${ST_OK}"
+    message="$(calc_tokens_health "${node_ring}")"
+    exit_formalities "${message}" "$?"
 fi
